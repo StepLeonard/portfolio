30596a73d1802bd422ffb34e2c6da75139705f80
stepleonard <stephanie.leonard716@gmail.com>
2026-04-09 21:05:45 -0700

    edited text

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,10 @@ function App() {
           Hi, I’m <span>Stephanie</span>
         </h1>
 
-        <p className="hero-subtitle"></p>
+        <p className="hero-subtitle">
+          Focused on building fast, user-friendly web applications with
+          real-world functionality.
+        </p>
 
         <div className="btn-row">
           <a href="#projects" className="primary-btn">
@@ -83,18 +86,24 @@ function App() {
       <section id="about" className="section">
         <h2>About</h2>
         <div className="card">
-          <p>
-            <h3>
-              I’m a Full-Stack Developer focused on building responsive web
-              applications using React, Node.js, Express, and PostgreSQL. I have
-              experience developing RESTful APIs, managing application state,
-              and creating user-friendly interfaces that deliver real
-              functionality. </h3></p>
-              <p><h3>I’m continuously improving my skills through
-              hands-on projects and bring a strong work ethic, adaptability, and
-              a collaborative mindset to every project.
-            </h3>
-          </p>
+          <h3>
+            I’m a Full-Stack Developer focused on building responsive web
+            applications using React, Node.js, Express, and PostgreSQL. I have
+            experience developing RESTful APIs, managing application state, and
+            creating user-friendly interfaces that deliver real functionality.
+          </h3>
+
+          <h3>
+            I’m continuously improving my skills through hands-on projects and
+            bring a strong work ethic, adaptability, and a collaborative mindset
+            to every project.
+          </h3>
+
+          <h3>
+            Experience collaborating on team-based projects using GitHub
+            workflows, including pull requests, code reviews, and version
+            control best practices.
+          </h3>
         </div>
       </section>
 
@@ -111,15 +120,68 @@ function App() {
       </section>
 
       <section id="projects" className="section">
-        
         <h2>Projects</h2>
         <h3>
           A selection of full-stack and frontend applications demonstrating
-          experience with React, RESTful APIs, and modern web development
-          practices.
+          experience building responsive interfaces, RESTful APIs, dynamic state
+          management, and modern web development workflows.
         </h3>
 
         <div className="projects-grid">
+          <div className="project-card">
+            <h3>Countries App</h3>
+            <p>
+              Built a full-stack web application using React, Node.js, Express,
+              and PostgreSQL, integrating external APIs and custom backend
+              endpoints to manage country data, user submissions, and view
+              tracking. Implemented dynamic routing, state management, and
+              RESTful API architecture to deliver a scalable and interactive
+              user experience.
+            </p>
+
+            <div className="project-buttons">
+              <a
+                href="https://version-4-countries.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+                className="project-btn live-btn"
+              >
+                Live Site
+              </a>
+              <a
+                href="https://github.com/StepLeonard/countries-app"
+                target="_blank"
+                rel="noreferrer"
+                className="project-btn github-btn"
+              >
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+
+          <div className="project-card">
+            <h3>Food Trucks App</h3>
+
+            <p>
+              Developed a full-stack food truck application using Express and
+              PostgreSQL, implementing RESTful API endpoints for retrieving,
+              filtering, and managing data. Collaborated in a team environment
+              using GitHub workflows, including pull requests, code reviews, and
+              version control best practices.
+            </p>
+
+            <div className="project-buttons">
+              <a
+                href="https://github.com/StepLeonard/food-trucks-app"
+                target="_blank"
+                rel="noreferrer"
+                className="project-btn github-btn"
+              >
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+
           <div className="project-card">
             <h3>Extension Management Dashboard</h3>
 
@@ -184,82 +246,6 @@ function App() {
           </div>
 
           <div className="project-card">
-            <h3>Countries App</h3>
-            <p>
-              Built a full-stack web application using React, Node.js, Express,
-              and PostgreSQL, integrating external APIs and custom backend
-              endpoints to manage country data, user submissions, and view
-              tracking. Implemented dynamic routing, state management, and
-              RESTful API architecture to deliver a scalable and interactive
-              user experience.
-            </p>
-
-            <div className="project-buttons">
-              <a
-                href="https://version-4-countries.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-                className="project-btn live-btn"
-              >
-                Live Site
-              </a>
-              <a
-                href="https://github.com/StepLeonard/countries-app"
-                target="_blank"
-                rel="noreferrer"
-                className="project-btn github-btn"
-              >
-                GitHub Repo
-              </a>
-            </div>
-          </div>
-
-          <div className="project-card">
-            <h3>Product Feedback App</h3>
-            <p>
-              ⚠️Currently developing a full-stack product feedback platform
-              designed to collect, manage, and analyze user input. Focused on
-              building scalable backend architecture, dynamic UI updates, and
-              efficient state management to support real-time
-              interaction.⚠️{" "}
-            </p>
-            <br></br>
-            <br></br>
-
-            <div className="project-buttons">
-              <a href="#" className="project-btn live-btn">
-                Live Site
-              </a>
-              <a href="#" className="project-btn github-btn">
-                GitHub Repo
-              </a>
-            </div>
-          </div>
-
-          <div className="project-card">
-            <h3>Food Trucks App</h3>
-
-            <p>
-              Developed a full-stack food truck application using Express and
-              PostgreSQL, implementing RESTful API endpoints for retrieving,
-              filtering, and managing data. Collaborated in a team environment
-              using GitHub workflows, including pull requests, code reviews, and
-              version control best practices.
-            </p>
-
-            <div className="project-buttons">
-              <a
-                href="https://github.com/StepLeonard/food-trucks-app"
-                target="_blank"
-                rel="noreferrer"
-                className="project-btn github-btn"
-              >
-                GitHub Repo
-              </a>
-            </div>
-          </div>
-
-          <div className="project-card">
             <h3>Developer Portfolio</h3>
 
             <p>
@@ -276,6 +262,27 @@ function App() {
                 rel="noreferrer"
                 className="project-btn github-btn"
               >
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+
+          <div className="project-card">
+            <h3>Product Feedback App</h3>
+            <p>
+              ⚠️Currently developing a full-stack product feedback platform
+              designed to collect, manage, and analyze user input. Focused on
+              building scalable backend architecture, dynamic UI updates, and
+              efficient state management to support real-time interaction.⚠️
+            </p>
+            <br></br>
+            <br></br>
+
+            <div className="project-buttons">
+              <a href="#" className="project-btn live-btn">
+                Live Site
+              </a>
+              <a href="#" className="project-btn github-btn">
                 GitHub Repo
               </a>
             </div>
